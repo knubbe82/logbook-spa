6911dece85f70352903abe2f7d833c94f0886f15
knubbe <knubbe1982@gmail.com>
2019-06-19 19:59:56 +0200

    seeders

--- database/factories/DiveFactory.php
+++ database/factories/DiveFactory.php
@@ -7,6 +7,12 @@ use Faker\Generator as Faker;
 
 $factory->define(Dive::class, function (Faker $faker) {
     return [
-        //
+        'title' => $faker->sentence,
+        'location' => $faker->address,
+        'time' => $faker->dateTimeBetween('-1 year', 'now'),
+        'description' => $faker->paragraph,
+        'user_id' => function() {
+            return App\User::all()->random();
+        }
     ];
 });
