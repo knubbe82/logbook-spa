25aae04ae1a0f55859cdd5bda8832838ef1c3b77
knubbe <knubbe1982@gmail.com>
2019-06-19 22:12:20 +0200

    jwt

--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,16 @@
 use App\Model\Dive;
 
 Route::apiResource('/dive', 'DiveController');
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
