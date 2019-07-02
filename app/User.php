76996f26723895217746d3e873bcf1ff5dd73ffc
knubbe <knubbe1982@gmail.com>
2019-07-02 17:55:03 +0200

    dives for auth user only

--- app/User.php
+++ app/User.php
@@ -40,7 +40,7 @@ class User extends Authenticatable implements JWTSubject
         'email_verified_at' => 'datetime',
     ];
 
-    public function dive()
+    public function dives()
     {
         return $this->hasMany(Dive::class);
     }
